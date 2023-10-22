de5a7f5a82ca6c92ce78c67642c9bef2104a2e0a
LordCode <9999269010dddd@gmail.com>
2023-10-22 11:29:28 +0300

    add tags

--- cogs/tags.py
+++ cogs/tags.py
@@ -5,25 +5,44 @@ class tags(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(invoke_without_command=True)
-    async def tag(self, ctx: commands.Context):
+    @staticmethod
+    def is_post(ctx: commands.Context):
         if ctx.channel.type!=nextcord.enums.ChannelType.public_thread:
-            return
+            raise Exception('No post')
         if ctx.channel.parent.type!=nextcord.enums.ChannelType.forum:
-            return
-        print("yes")
-    
-    @tag.command()
-    async def add(self,ctx: commands.Context, tag_name):
+            raise Exception('No forum')
+        return True
+
+    @commands.group(invoke_without_command=False)
+    @commands.check(is_post)
+    async def tag(self, ctx: commands.Context):
         pass
     
     @tag.command()
-    async def remove(self,ctx: commands.Context, tag_name):
-        pass
+    async def add(self,ctx: commands.Context,*,tag_name):
+        await ctx.message.delete()
+        tags = ctx.channel.applied_tags
+        for tagn in ctx.channel.parent.available_tags:
+            if tagn.name.lower() == tag_name.lower():
+                tag = tagn
+        if tag not in tags:
+            tags.append(tag)
+        await ctx.channel.edit(applied_tags=tags)
+        await ctx.send("Ok",delete_after=5)
     
     @tag.command()
-    async def set(self,ctx: commands.Context, tag_name):
-        pass
+    async def remove(self,ctx: commands.Context,*,tag_name):
+        await ctx.message.delete()
+        tags = ctx.channel.applied_tags
+        tag = None
+        for tagn in ctx.channel.parent.available_tags:
+            if tagn.name.lower() == tag_name.lower():
+                tag = tagn
+        if tag in tags:
+            tags.remove(tag)
+        await ctx.channel.edit(applied_tags=tags)
+        await ctx.send("Ok",delete_after=5)
+        
 
 
 def setup(bot):
