81f691cbb61e118c447984787e049fb2da268788
LordCode <9999269010dddd@gmail.com>
2023-11-10 21:37:23 +0300

    (test): delete

--- bot/views/views.py
+++ bot/views/views.py
@@ -7,11 +7,3 @@ from bot.databases.db import GuildDateBases
 from bot.misc.yandex_api import Track
 from typing import Any, Coroutine, List, Optional
 
-class CustomList(menus.Main):
-    dem = nextcord.Embed(title='Описание',description='Нашего персонала')
-    
-    async def callback(self,button: nextcord.ui.Button, interaction: nextcord.Interaction):
-        gem = self.dem
-        gem._fields = [self.value[self.index]]
-        await interaction.message.edit(embed=gem,view=self)
-
