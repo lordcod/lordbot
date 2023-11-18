6200f87311a7b4a83b40469ed185a7edfbb3d068
LordCode <9999269010dddd@gmail.com>
2023-11-19 00:08:20 +0300

    (prefix-sets): add langs

---
+++ bot/languages/settings.py
@@ -0,0 +1,41 @@
+
+class button:
+    back = {
+        'ru':'Назад',
+        'en':'Back'
+    }
+    edit = {
+        'ru':'Изменить',
+        'en':'Edit'
+    }
+    reset = {
+        'ru':'Сбросить',
+        'en':'Reset'
+    }
+
+class prefix:
+    title = {
+        'ru':'Префикс',
+        'en':'Prefix'
+    }
+    new_prefix = {
+        'ru':'Новый префикс',
+        'en':'New prefix'
+    }
+    description = {
+        'ru':'Установка префикса, на который бот будет реагировать при вызове команды.',
+        'en':'Setting the prefix to which the bot will respond when calling the command.'
+    }
+    current = {
+        'ru':'Текущий префикс',
+        'en':'Current prefix'
+    }
+    reset = {
+        'ru':'Префикс был сброшен',
+        'en':'Prefix has been reset'
+    }
+    default = {
+        'ru':'Префикс по умолчанию',
+        'en':'Default prefix'
+    }
+
--- bot/views/settings/prefix.py
+++ bot/views/settings/prefix.py
@@ -1,67 +1,90 @@
-from typing import Optional
 import nextcord
 from bot.databases.db import GuildDateBases
 from ..settings import DefaultSettingsView
 from bot.views import views
 from bot.resources.info import DEFAULT_PREFIX
 from bot.misc.utils import get_prefix
+from bot.languages.settings import (
+    prefix as prefix_langs,
+    button as button_name
+)
 
 
 class Modal(nextcord.ui.Modal):
-    type = 'modal'
-    
     def __init__(self,guild_id) -> None:
+        gdb = GuildDateBases(guild_id)
+        locale = gdb.get('language')
         prefix = get_prefix(guild_id,markdown=True)
         
-        super().__init__(title='Префикс')
+        super().__init__(title=prefix_langs.title.get(locale,'en'))
         
         self.prefix = nextcord.ui.TextInput(
-            label='Префикс:',
+            label=f'{prefix_langs.title.get(locale)}:',
             placeholder=prefix,
-            max_length=7
+            max_length=3
         )
         self.add_item(self.prefix)
     
     async def callback(self, interaction: nextcord.Interaction):
         prefix = self.prefix.value
         gdb = GuildDateBases(interaction.guild_id)
+        locale = gdb.get('language')
         gdb.set('prefix',prefix)
         
-        await interaction.response.send_message(f'New prefix - `{prefix}`',ephemeral=True)
+        await interaction.response.send_message(f"{prefix_langs.new_prefix.get(locale)} - `{prefix}`",ephemeral=True)
 
 class PrefixView(DefaultSettingsView):
-    embed = nextcord.Embed(
-        title='Префикс',
-        description='Установка префикса, на который бот будет реагировать при вызове команды.'
-    )
+    embed: nextcord.Embed
     
     def __init__(self,guild: nextcord.Guild) -> None:
         gdb = GuildDateBases(guild.id)
-        colour = gdb.get('color',1974050)
         prefix = get_prefix(guild.id,markdown=False)
+        colour = gdb.get('color')
+        locale = gdb.get('language')
         
-        self.embed.color = colour
-        self.embed._fields = [{'name':f'Текущий префикс: `{prefix}`','value':''}]
+        self.embed = nextcord.Embed(
+            title=prefix_langs.title.get(locale),
+            description=prefix_langs.description.get(locale),
+            color = colour
+        )
+        self.embed._fields = [
+            {
+                'name':f"{prefix_langs.current.get(locale)}: `{prefix}`",
+                'value':''
+            }
+        ]
         
         super().__init__()
+        
+        
+        self.back.label = button_name.back.get(locale)
+        self.edit.label = button_name.edit.get(locale)
+        self.reset.label = button_name.reset.get(locale)
     
     
-    @nextcord.ui.button(label='Назад',style=nextcord.ButtonStyle.red)
+    @nextcord.ui.button(label='Back',style=nextcord.ButtonStyle.red)
     async def back(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
         view = views.SettingsView(interaction.user)
         
         await interaction.message.edit(embed=view.embed,view=view)
     
-    @nextcord.ui.button(label='Изменить',style=nextcord.ButtonStyle.blurple)
+    @nextcord.ui.button(label='Edit',style=nextcord.ButtonStyle.blurple)
     async def edit(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
         modal = Modal(interaction.guild_id)
         
         await interaction.response.send_modal(modal)
     
-    @nextcord.ui.button(label='Сбросить',style=nextcord.ButtonStyle.success)
+    @nextcord.ui.button(label='Reset',style=nextcord.ButtonStyle.success)
     async def reset(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
         gdb = GuildDateBases(interaction.guild_id)
+        locale = gdb.get('language','en')
         prefix = DEFAULT_PREFIX
         
         gdb.set('prefix',prefix)
-        await interaction.response.send_message(f'Reset Prefix\nDefault prefix - `{prefix}`',ephemeral=True)
+        await interaction.response.send_message(
+        content=(
+            f"{prefix_langs.reset.get(locale)}\n"
+            f"{prefix_langs.default.get(locale)} - `{prefix}`"
+        ),
+        ephemeral=True
+        )
