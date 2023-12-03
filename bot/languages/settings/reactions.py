fd9612d78522c4d4db841abb58c72e9c4438f55b
LordCode <9999269010dddd@gmail.com>
2023-12-03 08:53:19 +0300

    change file settins to folder settings, add files

---
+++ bot/languages/settings/reactions.py
@@ -0,0 +1,61 @@
+
+
+class init:
+    title = {
+        'ru':'Автоматические реакции',
+        'en':'Automatic reactions'
+    }
+    brief_title = {
+        'ru':'Авто-реакции',
+        'en':'Auto-reactions'
+    }
+    description = {
+        'ru':'Управление автоматическим добавлением реакций в каналах',
+        'en':'Control of automatic addition of reactions in channels'
+    }
+    emoji = {
+        'ru':'Эмодзи',
+        'en':'Emoji'
+    }
+    channel = {
+        'ru':'Канал',
+        'en':'Channel'
+    }
+    ph = {
+        'ru':'Настройки авто-реакций:',
+        'en':'Auto-reaction settings:'
+    }
+
+class addres:
+    ph = {
+        'ru':'Выберете канал для авто-реакций',
+        'en':'Select a channel for auto-reactions'
+    }
+    channel_error = {
+        'ru':'На этот канал уже установлены авто-реакции',
+        'en':'Auto-reactions have already been installed on this channel'
+    }
+    install_emoji = {
+        'ru':'Установить эмодзи',
+        'en':'Install emoji'
+    }
+
+class datas:
+    delreact = {
+        'ru':'Удалить реакцию',
+        'en':'Delete reaction'
+    }
+    editreact = {
+        'ru':'Редактировать реакцию',
+        'en':'Edit reaction'
+    }
+
+class modal:
+    title = {
+        'ru':'Авто-реакция',
+        'en':'Auto reaction'
+    }
+    label = {
+        'ru':'Эмодзи:',
+        'en':'Emoji:'
+    }
