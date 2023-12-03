99fa5f9f651c53d10d5df501cee195bc2b431201
LordCode <9999269010dddd@gmail.com>
2023-12-03 08:47:52 +0300

    add auto role and welcome message

---
+++ bot/views/settings/greeting/role.py
@@ -0,0 +1,10 @@
+import nextcord
+
+from ...settings import DefaultSettingsView
+
+from bot.resources.ether import Emoji
+from bot.views import views
+from bot.databases.db import GuildDateBases
+from bot.languages.settings import (
+    button as button_name
+)
