ac5eff0f368c99de0c9103502a2f426479a29865
LordCode <9999269010dddd@gmail.com>
2024-02-04 22:22:48 +0300

    (settings): change default view

--- bot/views/settings/__init__.py
+++ bot/views/settings/__init__.py
@@ -1,3 +1,4 @@
+from ._view import DefaultSettingsView
 from .ideas import IdeasView
 from .welcomer import WelcomerView
 from .auto_role import AutoRoleView
@@ -8,31 +9,17 @@ from .languages import Languages
 from .prefix import PrefixView as Prefix
 from .economy import Economy
 from .color import ColorView as Color
-import nextcord
-
-
-class DefaultSettingsView(nextcord.ui.View):
-    async def interaction_check(self, interaction: nextcord.Interaction) -> bool:
-        if not interaction.user.guild_permissions.manage_guild:
-            await interaction.response.send_message('You don\'t have the authority to use the settings', ephemeral=True)
-            return False
-        return True
 
 
 moduls = {
     'Economy': Economy,
-
     'Color': Color,
     'Languages': Languages,
     'Prefix': Prefix,
-
-    'CommandPermission': CommandsDataView,
-
+    'CommandPermission':   CommandsDataView,
     'Welcomer': WelcomerView,
     'AutoRoles': AutoRoleView,
-
     'Reactions': AutoReactions,
     'ThreadMessage': AutoThreadMessage,
-
     'Ideas': IdeasView,
 }
