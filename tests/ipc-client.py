245b9b2a2b5f0452fa261018cf9ef90e6c820072
LordCode <9999269010dddd@gmail.com>
2024-02-09 20:10:04 +0300

    add ipc

---
+++ tests/ipc-client.py
@@ -0,0 +1,21 @@
+import asyncio
+from nextcord.ext import ipc
+
+
+
+async def main():
+    ipc_client = ipc.Client(host="192.168.1.15", secret_key="my_secret_key")  # secret_key must be the same as your server
+    
+    await asyncio.sleep(5)
+    
+    member_count = await ipc_client.request("get_member_count", guild_id=1165681101294030898)
+    
+    print(member_count)
+    
+    await asyncio.sleep(5)
+    
+    await ipc_client.session.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
