6723a21cf3b541bbe1d98220b17d810407eab2b2
LordCode <9999269010dddd@gmail.com>
2024-02-18 15:51:13 +0300

    fix(token): change anprim token

--- bot/misc/env.py
+++ bot/misc/env.py
@@ -3,12 +3,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-anprim_token = "MTE2Mzc0NzQxOTg2ODExOTA0MA.GA6nMN.oGDmsgTtQX-40ov51airPeziHflOg7Z2_XNSt0"
-
-token_lord_cord = environ.get("lordcord_token")
-token_lord_kind = environ.get("lordkind_token")
-token_lord_classic = environ.get("lordclassic_token")
 
 
 class Tokens:
+    token_anprim = environ.get("anprim_token")
+    token_lord_cord = environ.get("lordcord_token")
+    token_lord_kind = environ.get("lordkind_token")
+    token_lord_classic = environ.get("lordclassic_token")
     token = token_lord_cord
