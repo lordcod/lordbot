f39b700506328dfdae3c663b5c4733b82f0ff3c7
LordCode <9999269010dddd@gmail.com>
2024-03-10 10:27:40 +0300

    feat(to_i18n_format): add func in i18n

--- bot/languages/i18n.py
+++ bot/languages/i18n.py
@@ -11,7 +11,7 @@ resource_dict = {}
 
 
 def _load_file(filename: str) -> bytes:
-    with open(filename, 'rb') as f:
+    with open(filename, "rb") as f:
         return f.read()
 
 
@@ -20,6 +20,7 @@ def _parse_json(content: str) -> dict:
 
 
 def add_res_translation(key: str, value: str, locale: str):
+    resource_dict.setdefault(locale, {})
     data = resource_dict[locale]
     data_keys = key.split(".")
     for num, tk in enumerate(data_keys, start=1):
@@ -31,8 +32,10 @@ def add_res_translation(key: str, value: str, locale: str):
         data = data[tk]
 
 
-def add_translation(key: str, value: str, locale: Optional[str] = None, loadable: bool = False) -> None:
-    locale = locale or config.get('locale')
+def add_translation(
+    key: str, value: str, locale: Optional[str] = None, loadable: bool = False
+) -> None:
+    locale = locale or config.get("locale")
     memoization_dict.setdefault(locale, {})
     memoization_dict[locale][key] = value
     if not loadable:
@@ -53,10 +56,9 @@ def translate_dict(src: str, dest: str, src_dict: dict) -> dict:
         elif isinstance(value, list):
             for num, text in enumerate(value):
                 dest_dict.setdefault(key, [])
-                dest_dict[key][num] = translator.translate(
-                    text, dest, src).text
+                dest_dict[key][num] = translator.translate(text, dest, src).text
         else:
-            dest_dict[key] = translator.translate(value,  dest, src).text
+            dest_dict[key] = translator.translate(value, dest, src).text
     return dest_dict
 
 
@@ -95,6 +97,16 @@ def to_any_locales() -> dict:
     return new_data
 
 
+def to_i18n_translation(data: dict, path: Optional[str] = None) -> None:
+    for key, value in data.items():
+        if not isinstance(value, dict):
+            raise ValueError("Use another method")
+        if set(default_languages) & set(value.keys()):
+            add_dict_translations(f"{path+'.' if path else ''}{key}", value)
+        else:
+            to_i18n_translation(value, f"{path+'.' if path else ''}{key}")
+
+
 def from_folder(foldername: str) -> None:
     for filename in os.listdir(foldername):
         if not filename.endswith(".json"):
@@ -112,29 +124,28 @@ def to_folder(foldername: str) -> str:
             file.write(jsondata)
 
 
-def parser(json_resource: dict, locale: Optional[str] = None, prefix: Optional[str] = None, loadable: bool = True) -> None:
+def parser(
+    json_resource: dict,
+    locale: Optional[str] = None,
+    prefix: Optional[str] = None,
+    loadable: bool = True,
+) -> None:
     for key, value in json_resource.items():
         if isinstance(value, dict):
             parser(
-                value,
-                locale,
-                f"{prefix+'.' if prefix else ''}{key}",
-                loadable=loadable
+                value, locale, f"{prefix+'.' if prefix else ''}{key}", loadable=loadable
             )
         else:
             add_translation(
-                f"{prefix+'.' if prefix else ''}{key}", value, locale, loadable=loadable)
+                f"{prefix+'.' if prefix else ''}{key}", value, locale, loadable=loadable
+            )
 
 
-def t(
-    locale: Optional[str] = None,
-    path: Optional[str] = "",
-    **kwargs
-) -> str:
+def t(locale: Optional[str] = None, path: Optional[str] = "", **kwargs) -> str:
     if locale not in memoization_dict:
-        locale = config.get('locale')
+        locale = config.get("locale")
     if path not in memoization_dict[locale]:
-        data = memoization_dict[config.get('locale')][path]
+        data = memoization_dict[config.get("locale")][path]
     else:
         data = memoization_dict[locale][path]
 
@@ -144,6 +155,7 @@ def t(
 if __name__ == "__main__":
     from_folder("./bot/languages/localization")
 
+    # Translation dict
     # for lang in default_languages:
     #     if lang == "en":
     #         continue
@@ -153,18 +165,37 @@ if __name__ == "__main__":
     #     print(trd)
     #     parser(trd, lang, "settings.music", loadable=False)
 
+    # Translate to default languages
     # data = translation_with_languages(
     #     "en", "Issues a sheet with all temporary bans that were issued by the bot", default_languages)
     # print(orjson.dumps(data).decode())
 
+    # Translation to default languages and added
     # add_dict_translations(
     #     "help.record", translation_with_languages("en", "Record", default_languages))
 
+    # To any locales format
     # data = to_any_locales()
-    # with open("test_loc.json", "wb") as file:
+    # with open("test_loc.json", "+wb") as file:
     #     jsondata = orjson.dumps(data)
     #     file.write(jsondata)
 
-    print(t("ideas.confirm-view.button.approve"))
+    # To i18n format as any locales format
+    # to_i18n_translation(
+    #     {
+    #         "failed": {
+    #             "da": "Rollen {role} kan ikke tildeles og bruges til integration eller af en bot.",
+    #             "de": "Die Rolle {role} kann nicht zugewiesen werden und wird zur Integration oder von einem Bot verwendet.",
+    #             "en": "The {role} role cannot be assigned and is used for integration or by a bot.",
+    #             "es": "El rol {role} no se puede asignar y se usa para la integración o por un bot.",
+    #             "fr": "Le rôle {role} ne peut pas être attribué et est utilisé pour l'intégration ou par un bot.",
+    #             "id": "Peran {role} tidak dapat ditetapkan dan digunakan untuk integrasi atau dengan bot.",
+    #             "pl": "Rola {role} nie można przypisać i służy do integracji lub przez bot.",
+    #             "ru": "Роль {role} не может быть назначена, потому что используется для интеграции или бота.",
+    #             "tr": "{role} rolü atanamaz ve entegrasyon veya bir bot tarafından kullanılır.",
+    #         }
+    #     }
+    # )
+    # print(resource_dict)
 
     # to_folder("./bot/languages/localization")
