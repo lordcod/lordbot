a69b99ea05e162cced7942d7d270c1851bd8e4f2
LordCode <9999269010dddd@gmail.com>
2024-03-20 20:37:06 +0300

    feat(voice-time): add time counter in voice

---
+++ bot/cogs/events/voice_state.py
@@ -0,0 +1,43 @@
+
+import time
+import nextcord
+from nextcord.ext import commands
+
+from bot.databases import localdb
+from bot.misc.lordbot import LordBot
+
+
+VOICE_STATE_DB = localdb.get_table('voice_state')
+EXP_STATE_DB = localdb.get_table('exps')
+TEMP_VOICE_STATE_DB = {}
+
+
+class voice_state_event(commands.Cog):
+    def __init__(self, bot: LordBot) -> None:
+        self.bot = bot
+        super().__init__()
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: nextcord.Member, before: nextcord.VoiceState, after: nextcord.VoiceState) -> None:
+        if before.channel is None and after.channel is not None:
+            await self.connect_to_voice(member)
+        if before.channel is not None and after.channel is None:
+            await self.disconnect_from_voice(member)
+
+    async def connect_to_voice(self, member: nextcord.Member) -> None:
+        TEMP_VOICE_STATE_DB[member.id] = time.time()
+
+    async def disconnect_from_voice(self, member: nextcord.Member) -> None:
+        member_started_at = TEMP_VOICE_STATE_DB.get(member.id)
+
+        if member_started_at is None:
+            return
+
+        voice_time = time.time()-member_started_at
+        total_voice_time = VOICE_STATE_DB.get(member.id, 0)
+
+        VOICE_STATE_DB[member.id] = total_voice_time+voice_time
+
+
+def setup(bot):
+    bot.add_cog(voice_state_event(bot))
--- bot/databases/localdb.py
+++ bot/databases/localdb.py
@@ -1,9 +1,23 @@
+from typing import TypeVar, overload
+
+T = TypeVar("T")
+
 data = {}
 
 
-def get_table(table_name):
+@overload
+def get_table(table_name, default: T) -> T:
+    ...
+
+
+@overload
+def get_table(table_name) -> dict:
+    ...
+
+
+def get_table(table_name, default: T = None) -> T:
     if table_name not in data:
-        data[table_name] = {}
+        data[table_name] = {} if default is None else default
     return data[table_name]
 
 
