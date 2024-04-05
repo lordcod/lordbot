33d66f94573909050f4061e5e65d81186c32efff
LordCode <9999269010dddd@gmail.com>
2024-04-05 21:56:26 +0300

    test: add cripter

--- tests/cripter.py
+++ tests/cripter.py
@@ -5,14 +5,20 @@ import random
 def encrypt(message: bytes, key: bytes) -> bytes:
     return Fernet(key).encrypt(message)
 
-
 def decrypt(token: bytes, key: bytes) -> bytes:
     return Fernet(key).decrypt(token)
 
+def randfloat(a: float | int, b: float | int, scope: int = 14) -> float:
+    return random.randint(int(a*10**scope), int(b*10**scope)) / 10**scope
 
-message = str(random.randint(1, 10)).encode()
-key = Fernet.generate_key()
-token = encrypt(message, key)
-res = decrypt(token, key).decode()
 
-print(res)
+# def randnumtk(a: float | int, b: float | int) -> tuple[bytes, bytes]:
+#     key = Fernet.generate_key()
+#     return encrypt(str(randfloat(a, b)).encode(), key), key
+# print(decrypt(*randnumtk(1,10)))
+
+# message = str(randfloat(99.9, 100.0, 100)).encode()
+# key = Fernet.generate_key()
+# token = encrypt(message, key)
+# res = decrypt(token, key)
+
