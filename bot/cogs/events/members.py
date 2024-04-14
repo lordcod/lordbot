870df37a38208097f60deb1ef5ed0d53e4d718f9
LordCode <9999269010dddd@gmail.com>
2024-04-14 18:35:11 +0300

    refactor: members event

--- bot/cogs/events/members.py
+++ bot/cogs/events/members.py
@@ -5,7 +5,8 @@ from bot.misc import utils
 from bot.misc.logger import Logger
 from bot.databases import GuildDateBases
 
-import functools 
+import functools
+import asyncio
 import jmespath
 import time
 from typing import Dict, List, Optional
@@ -32,11 +33,12 @@ class MembersEvent(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member: nextcord.Member):
         gdb = GuildDateBases(member.guild.id)
-        
-        
+
         invite = await self.process_invites(member, gdb)
-        self.bot.loop.create_task(self.auto_roles(member, gdb))
-        self.bot.loop.create_task(self.auto_message(member, invite, gdb))
+        await asyncio.gather(
+            self.auto_roles(member, gdb),
+            self.auto_message(member, invite, gdb)
+        )
 
     @on_error
     async def auto_roles(self, member: nextcord.Member, gdb: GuildDateBases):
@@ -46,7 +48,7 @@ class MembersEvent(commands.Cog):
             return
 
         roles = filter(lambda item: item is not None,
-                       [member.guild.get_role(role_id) for role_id in roles_ids])
+                       map(member.guild.get_role, roles_ids))
 
         await member.add_roles(*roles, atomic=False)
 
@@ -54,7 +56,6 @@ class MembersEvent(commands.Cog):
     async def auto_message(self, member: nextcord.Member, invite: nextcord.Invite | None, gdb: GuildDateBases):
         guild = member.guild
         greeting_message: dict = gdb.get('greeting_message', {})
-
 
         if not (channel := guild.get_channel(greeting_message.get("channel_id"))):
             return
@@ -92,17 +93,18 @@ class MembersEvent(commands.Cog):
             return
 
         invites = gdb.get('invites', [])
-        invites.append((member.id, invite.inviter.id, time.time(), invite.code))
+        invites.append((member.id, invite.inviter.id,
+                       time.time(), invite.code))
         gdb.set('invites', invites)
-        
+
         return invite
 
-    @commands.command()
+    @ commands.command()
     async def invites(self, ctx: commands.Context, member: Optional[nextcord.Member] = None):
         gdb = GuildDateBases(ctx.guild.id)
         color = gdb.get('color')
         invites = gdb.get('invites', [])
-        
+
         if member is None:
             embed = nextcord.Embed(
                 title="Invites",
@@ -112,10 +114,11 @@ class MembersEvent(commands.Cog):
             for member_id, invitor_id, _, code in invites:
                 member = ctx.guild.get_member(member_id)
                 invitor = ctx.guild.get_member(invitor_id)
-                 
+
                 embed.description += f"{invitor.mention if invitor else '**'+invitor_id+'**'} → {member.mention if member else '**'+member_id+'**'} | `{code}`"
         else:
-            member_invites = jmespath.search(f"[?@[1]==`{member.id}`]", invites) or []
+            member_invites = jmespath.search(
+                f"[?@[1]==`{member.id}`]", invites) or []
             embed = nextcord.Embed(
                 title=f"Invites of {member.display_name}",
                 description="",
@@ -123,12 +126,12 @@ class MembersEvent(commands.Cog):
             )
             for member_id, _, add_at, code in member_invites:
                 member = ctx.guild.get_member(member_id)
-                    
+
                 embed.description += f"<t:{add_at :.0f}:f> | {member.mention if member else '**'+member_id+'**'} | `{code}`"
-        
+
         if not embed.description:
             embed.description = "Information not found!"
-        
+
         await ctx.send(embed=embed)
 
 
