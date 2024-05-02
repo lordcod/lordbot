5a0182613fe7c5188c1dc6a968d8318c42e6aa36
LordCode <9999269010dddd@gmail.com>
2024-05-03 02:50:02 +0300

    feat(roulette): new game roulette

--- bot/cogs/economy.py
+++ bot/cogs/economy.py
@@ -1,5 +1,12 @@
+import asyncio
+import random
 import nextcord
 from nextcord.ext import commands
+
+
+import time
+from typing import Callable, Dict, List, Optional, Tuple, Union, Literal
+from itertools import product
 
 from bot.databases import EconomyMemberDB, GuildDateBases
 from bot.misc.lordbot import LordBot
@@ -8,10 +15,97 @@ from bot.resources.ether import Emoji
 from bot.misc.utils import get_award
 from nextcord.utils import escape_markdown
 
-import time
-from typing import Optional, Union, Literal
 
+roulette_games: Dict[int,
+                     Tuple[
+                         Callable[[], None],
+                         List[Tuple[nextcord.Member, int, str]],
+                         nextcord.Message]
+                     ] = {}
 timeout_rewards = {"daily": 86400, "weekly": 604800, "monthly": 2592000}
+arguments_roulette = [
+    {
+        "input_data_condition": lambda val: val.isdigit() and 0 <= val <= 36,
+        "random_condition": lambda val, ran: ran == val,
+        "multiplier": 35,
+    },
+    {
+        "input_data_condition": lambda val: val == "1 to 12" or val.replace(" ", "") == "1-12",
+        "random_condition": lambda val, ran: 1 <= ran <= 12,
+        "multiplier": 3,
+    },
+    {
+        "input_data_condition": lambda val: val == "13 to 24" or val.replace(" ", "") == "13-24",
+        "random_condition": lambda val, ran: 13 <= ran <= 24,
+        "multiplier": 3,
+    },
+    {
+        "input_data_condition": lambda val: val == "25 to 36" or val.replace(" ", "") == "25-36",
+        "random_condition": lambda val, ran: 25 <= ran <= 36,
+        "multiplier": 3,
+    },
+    {
+        "input_data_condition": lambda val: val == "1 to 18" or val.replace(" ", "") == "1-18",
+        "random_condition": lambda val, ran: 1 <= ran <= 18,
+        "multiplier": 2,
+    },
+    {
+        "input_data_condition": lambda val: val == "19 to 36" or val.replace(" ", "") == "19-36",
+        "random_condition": lambda val, ran: 19 <= ran <= 36,
+        "multiplier": 2,
+    },
+    {
+        "input_data_condition": lambda val: val == "red",
+        "random_condition": lambda val, ran: ran in [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36],
+        "multiplier": 2,
+    },
+    {
+        "input_data_condition": lambda val: val == "black",
+        "random_condition": lambda val, ran: ran in [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35],
+        "multiplier": 2,
+    },
+    {
+        "input_data_condition": lambda val: val == "even",
+        "random_condition": lambda val, ran: ran % 2 == 0,
+        "multiplier": 2,
+    },
+    {
+        "input_data_condition": lambda val: val == "odd",
+        "random_condition": lambda val, ran: ran % 2 == 1,
+        "multiplier": 2,
+    },
+    {
+        "input_data_condition": lambda val: val == "1st",
+        "random_condition": lambda val, ran: ran in [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34],
+        "multiplier": 3,
+    },
+    {
+        "input_data_condition": lambda val: val == "2nd",
+        "random_condition": lambda val, ran: ran in [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35],
+        "multiplier": 3,
+    },
+    {
+        "input_data_condition": lambda val: val == "3rd",
+        "random_condition": lambda val, ran: ran in [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36],
+        "multiplier": 3
+    },
+]
+
+
+def create_roulette_task():
+    loop = asyncio.get_event_loop()
+    future = loop.create_future()
+    _task = loop.call_later(10, future.set_result, None)
+
+    def wrapped():
+        nonlocal _task
+        _task.cancel()
+        _task = loop.call_later(10, future.set_result, None)
+    return future, wrapped
+
+
+def is_valid_roulette_argument(val: str) -> bool:
+    return any((data['input_data_condition'](val) for data in arguments_roulette))
 
 
 class economy(commands.Cog):
@@ -173,7 +267,7 @@ class economy(commands.Cog):
         await ctx.send(embed=embed, file=file_pedestal)
 
     @commands.command(name="pay")
-    async def pay(self, ctx: commands.Context, member: nextcord.Member, sum: int):
+    async def pay(self, ctx: commands.Context, member: nextcord.Member, amount: int):
         gdb = GuildDateBases(ctx.guild.id)
         color = gdb.get('color')
         prefix = gdb.get('prefix')
@@ -182,28 +276,28 @@ class economy(commands.Cog):
         from_account = EconomyMemberDB(ctx.guild.id, ctx.author.id)
         to_account = EconomyMemberDB(ctx.guild.id, member.id)
 
-        if sum <= 0:
+        if amount <= 0:
             await ctx.send(content="Specify the amount more **0**")
             return
-        elif (from_account.get('balance', 0)-sum) < 0:
+        elif (from_account.get('balance', 0)-amount) < 0:
             await ctx.send(content=f"Not enough funds to check your balance use `{prefix}bal`")
             return
 
         embed = nextcord.Embed(
             title="Transfer of currency",
             color=color,
-            description=f"You were given **{sum}**{currency_emoji}"
+            description=f"You were given **{amount}**{currency_emoji}"
         )
         embed.set_footer(
             text=f'From {ctx.author.display_name}', icon_url=ctx.author.display_avatar)
 
         await ctx.send(embed=embed)
 
-        from_account["balance"] -= sum
-        to_account["balance"] += sum
+        from_account["balance"] -= amount
+        to_account["balance"] += amount
 
     @commands.command(name="deposit", aliases=["dep"])
-    async def deposit(self, ctx: commands.Context, sum: Union[Literal['all'], int]):
+    async def deposit(self, ctx: commands.Context, amount: Union[Literal['all'], int]):
         gdb = GuildDateBases(ctx.guild.id)
         color = gdb.get('color')
         prefix = gdb.get('prefix')
@@ -212,22 +306,22 @@ class economy(commands.Cog):
         account = EconomyMemberDB(ctx.guild.id, ctx.author.id)
         balance = account.get('balance', 0)
 
-        if sum == "all":
-            sum = balance
+        if amount == "all":
+            amount = balance
 
-        if sum <= 0:
+        if amount <= 0:
             await ctx.send(content="Specify the amount more `0`")
             return
-        if (balance - sum) < 0:
+        if (balance - amount) < 0:
             await ctx.send(content=f"Not enough funds to check your balance use `{prefix}balance`")
             return
-        account['balance'] = account['balance'] - sum
-        account['bank'] = account['bank'] + sum
+        account['balance'] = account['balance'] - amount
+        account['bank'] = account['bank'] + amount
 
         embed = nextcord.Embed(
             title="Transfer of currency",
             color=color,
-            description=f"You have transferred **{sum}**{currency_emoji} to the bank account"
+            description=f"You have transferred **{amount}**{currency_emoji} to the bank account"
         )
         embed.set_footer(text=ctx.author.display_name,
                          icon_url=ctx.author.display_avatar)
@@ -235,7 +329,7 @@ class economy(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command(name="withdraw", aliases=["wd"])
-    async def withdraw(self, ctx: commands.Context, sum: Union[Literal['all'], int]):
+    async def withdraw(self, ctx: commands.Context, amount: Union[Literal['all'], int]):
         gdb = GuildDateBases(ctx.guild.id)
         color = gdb.get('color')
         prefix = gdb.get('prefix')
@@ -244,22 +338,22 @@ class economy(commands.Cog):
         account = EconomyMemberDB(ctx.guild.id, ctx.author.id)
         bank = account.get('bank', 0)
 
-        if sum == "all":
-            sum = bank
+        if amount == "all":
+            amount = bank
 
-        if sum <= 0:
+        if amount <= 0:
             await ctx.send(content="Specify the amount more `0`")
             return
-        if (bank - sum) < 0:
+        if (bank - amount) < 0:
             await ctx.send(content=f"Not enough funds to check your balance use `{prefix}balance`")
             return
-        account['balance'] += sum
-        account['bank'] -= sum
+        account['balance'] += amount
+        account['bank'] -= amount
 
         embed = nextcord.Embed(
             title="Transfer of currency",
             color=color,
-            description=f"You have transferred **{sum}**{currency_emoji} to the account"
+            description=f"You have transferred **{amount}**{currency_emoji} to the account"
         )
         embed.set_footer(text=ctx.author.display_name,
                          icon_url=ctx.author.display_avatar)
@@ -268,7 +362,7 @@ class economy(commands.Cog):
 
     @commands.command(name="gift")
     @commands.has_permissions(administrator=True)
-    async def gift(self, ctx: commands.Context, member: Optional[nextcord.Member], sum: int):
+    async def gift(self, ctx: commands.Context, member: Optional[nextcord.Member], amount: int):
         if not member:
             member = ctx.author
 
@@ -277,20 +371,20 @@ class economy(commands.Cog):
         currency_emoji = eco_sets.get('emoji')
         account = EconomyMemberDB(ctx.guild.id, member.id)
 
-        if sum > 1_000_000:
+        if amount > 1_000_000:
             await ctx.send(f"The maximum amount for this server - {1_000_000: ,}{currency_emoji}")
             return
-        if 0 >= sum:
+        if 0 >= amount:
             await ctx.send("The amount must be positive")
             return
 
-        account["balance"] += sum
+        account["balance"] += amount
 
-        await ctx.send(f"You passed {member.display_name}, **{sum}**{currency_emoji}")
+        await ctx.send(f"You passed {member.display_name}, **{amount}**{currency_emoji}")
 
     @commands.command(name="take")
     @commands.has_permissions(administrator=True)
-    async def take(self, ctx: commands.Context, member: Optional[nextcord.Member], sum: int):
+    async def take(self, ctx: commands.Context, member: Optional[nextcord.Member], amount: int):
         if not member:
             member = ctx.author
 
@@ -299,20 +393,109 @@ class economy(commands.Cog):
         currency_emoji = eco_sets.get('emoji')
         account = EconomyMemberDB(ctx.guild.id, member.id)
 
-        if sum > 1_000_000:
+        if amount > 1_000_000:
             await ctx.send(f"The maximum amount for this server - {1_000_000: ,}{currency_emoji}")
             return
-        if 0 >= sum:
+        if 0 >= amount:
             await ctx.send("The amount must be positive")
             return
 
-        if 0 > (account.get('balance')-sum):
+        if 0 > (account.get('balance')-amount):
             await ctx.send('The operation cannot be performed because the balance will become negative during it')
             return
 
-        account["balance"] -= sum
+        account["balance"] -= amount
 
-        await ctx.send(f"You passed `{member.display_name}`, **{sum}**{currency_emoji} ")
+        await ctx.send(f"You passed `{member.display_name}`, **{amount}**{currency_emoji} ")
+
+    @commands.command(name="roulette", aliases=["rou"])
+    async def roulette(self, ctx: commands.Context, amount: int, *, val: str):
+        gdb = GuildDateBases(ctx.guild.id)
+        prefix = gdb.get('prefix')
+        color = gdb.get('color')
+        account = EconomyMemberDB(ctx.guild.id, ctx.author.id)
+        val = val.lower()
+
+        if not is_valid_roulette_argument(val):
+            await ctx.send(content=f"An incorrect value has been entered for the roulette game, use the command `{prefix}help {ctx.command.name}` to find out the arguments")
+            raise TypeError('Planned error')
+        if amount <= 0:
+            await ctx.send(content="Specify the amount more `0`")
+            raise TypeError('Planned error')
+        if amount > account['balance']:
+            await ctx.send(content=f"Not enough funds to check your balance use `{prefix}balance`")
+            raise TypeError('Planned error')
+
+        account["balance"] -= amount
+
+        if rg := roulette_games.get(ctx.guild.id):
+            postpone, listener, mes = rg
+            postpone()
+            listener.append((ctx.author, amount, val))
+            embed = nextcord.Embed(
+                title="New roulette game started!",
+                description='\n'.join(
+                    [f"{member.mention} have placed a bet of {amount} on {val}" for member, amount, val in listener]),
+                color=color
+            )
+            embed.set_author(name=ctx.guild.name,
+                             icon_url=ctx.guild.icon)
+            embed.set_footer(
+                text="Time remaining: 10 seconds after each bet (maximum 1 minute)")
+            asyncio.create_task(mes.edit(embed=embed))
+            if ctx.channel == mes.channel:
+                asyncio.create_task(ctx.message.add_reaction(Emoji.success))
+            else:
+                asyncio.create_task(
+                    ctx.send(f"The game has already started, [go to the game]({mes.jump_url})"))
+            return
+        else:
+            embed = nextcord.Embed(
+                title="New roulette game started!",
+                description=f"{ctx.author.mention} have placed a bet of {amount} on {val}",
+                color=color
+            )
+            embed.set_author(name=ctx.guild.name,
+                             icon_url=ctx.guild.icon)
+            embed.set_footer(
+                text="Time remaining: 10 seconds after each bet (maximum 1 minute)")
+            mes = await ctx.send(embed=embed)
+
+            listener = [(ctx.author, amount, val)]
+            future, postpone = create_roulette_task()
+            roulette_games[ctx.guild.id] = (
+                postpone, listener, mes)
+
+            try:
+                await asyncio.wait_for(future, timeout=60)
+            except asyncio.TimeoutError:
+                pass
+
+        roulette_games.pop(ctx.guild.id, None)
+        ran = random.randint(1, 36)
+        results = []
+        for _member, _amount, _arg in listener:
+            for roulette_item in arguments_roulette:
+                if roulette_item["input_data_condition"](_arg):
+                    account = EconomyMemberDB(ctx.guild.id, _member.id)
+                    if roulette_item["random_condition"](_arg, ran):
+                        account["balance"] += _amount * \
+                            roulette_item["multiplier"]
+                        results.append(
+                            f"{_member.mention} won **{_amount * roulette_item['multiplier']}**")
+                    else:
+                        results.append(f"{_member.mention} lost **{_amount}**")
+                    break
+        win_color = 'red' if arguments_roulette[6]["random_condition"](
+            0, ran) else 'black'
+        embed = nextcord.Embed(
+            title=f"The ball landed on **{win_color} {ran}**",
+            description='\n'.join(results),
+            color=color
+        )
+        embed.set_author(name=ctx.guild.name,
+                         icon_url=ctx.guild.icon)
+        await mes.edit(embed=embed)
 
 
 def setup(bot):
