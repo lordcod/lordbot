791581d2d84348074f34e3622bcfbdf3b08523ba
LordCode <9999269010dddd@gmail.com>
2024-08-30 14:25:49 +0300

    fix: tokens

--- bot/misc/env.py
+++ bot/misc/env.py
@@ -1,11 +1,10 @@
-from enum import StrEnum
 from os import environ
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-class Tokens(StrEnum):
+class Tokens:
     token_anprim = environ.get("anprim_token")
     token_lordcord = environ.get("lordcord_token")
     token_lordkind = environ.get("lordkind_token")
